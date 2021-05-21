0c402928083f68340d7419d93563cbbeff5549bc
aliceyoo <jyynkr92@gmail.com>
2021-05-21 14:51:25 +0900

    result 텍스트 수정

--- src/data/content.ts
+++ src/data/content.ts
@@ -2,4 +2,4 @@ export const TEXT_RESET = '초기화';
 export const TEXT_START = '시작';
 export const TEXT_START_QUESTION = '문제 단어';
 export const TEXT_PLACEHOLDER = '입력';
-export const TEXT_RETRY = '다시하기';
+export const TEXT_RETRY = '다시 시작';
--- src/js/pages/result.ts
+++ src/js/pages/result.ts
@@ -5,7 +5,7 @@ const setResultHTML = ({ time, score }: { time: number; score: number }) => {
     <div class="result_wrapper">
       <h1>단어 타이핑 게임</h1>
       <div class="score_result">당신의 점수는 <span class="score">${score}</span>점입니다.</div>
-      <div class="time_result">평균 응답시간은 <span class="time">${time}</span>초입니다.</div>
+      <div class="time_result">단어당 평균 응답시간은 <span class="time">${time}</span>초입니다.</div>
       <div class="btn_wrapper">
         <button id="retry_btn">${TEXT_RETRY}</button>
       </div>
